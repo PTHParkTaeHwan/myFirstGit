0f4c1ba2408f5eeece127bedb3ea9422626d0643
PTHParkTaeHwan <pth0810@naver.com>
2020-01-30 17:39:46 +0900

    프로젝트관리 연습

---
+++ myFirstGit/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include"class/object.h"
+#include"sclass/sClass.h"
+#include""
+
+int main()
+{
+	somthing anyOne;
+	sClass anyTwo;
+	
+	
+	for (int i = 0; i < 10; i++)
+	{
+		anyOne.print();
+		//std::cout << "Hello World!\n";
+	}
+	return 0;
+}
---
+++ test1/src/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << i << " hello test \n";
+	}
+	return 0;
+}
